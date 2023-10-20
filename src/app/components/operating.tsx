abad10921af5bee73aec191c0f499f7647414a3b
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-10-20 15:50:14 -0700

    Added wait time to operating rides

--- src/app/components/operating.tsx
+++ src/app/components/operating.tsx
@@ -1,11 +1,23 @@
 export default function Operating({data}:any) {
 
-  const { name } = data
+  const { name, queue } = data
+  let wait = 0
+
+  try{
+    wait = queue.STANDBY.waitTime
+    if (wait == null) {
+      wait = 0
+    }
+  }
+  catch(TypeError){
+    wait = 0
+  }
 
   return(
     <div>
       <h1>{name}</h1>
       <h2>Operating</h2>
+      <h3>Wait - {wait}</h3>
     </div>
   )
 }
