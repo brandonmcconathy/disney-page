9e4c32ffe2d5ad7eb27dd24259c4d13a3b1e3e14
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-10-20 02:02:14 -0700

    added api call to get all attractions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,28 @@
+'use client'
 
+import axios from "axios"
+import { useState, useEffect } from "react"
 
 export default function Home() {
+
+  const [data, setData] = useState([])
+
+  useEffect( () => {
+
+    axios.get('https://api.themeparks.wiki/v1/entity/7340550b-c14d-4def-80bb-acdb51d49a66/children').then((response) => {
+      let temp : Array<object> = []
+      response.data.children.map( (attraction: any) => {
+        if (attraction.entityType == 'ATTRACTION') {
+          temp.push(attraction)
+        }
+      })
+      setData(temp)
+    })
+
+  },[])
+
+  console.log(data)
+
   return (
     <div>
 
